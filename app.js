471a6dd3eb1a475d1f4849d167398b23df547aab
Alex Liu <yuningalexliu@gmail.com>
2015-02-21 21:53:58 -0500

    started parsing feed

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+//Application Requirements
+
 var express = require('express')
 var http = require('http')
 var ProtoBuf = require('protobufjs')
+var _ = require('underscore')
 
-//Fetch data from MTA
+//API Configuration
 
 var options = {
 	host: 'datamine.mta.info',
 	path: '/mta_esi.php?key=7aae2644bbaa198a716929cbdf99d2f3&feed_id=1'
 }
 
+//Fetch the data
+
+var currentLine = "2"
 var mtaData = '';
+var transit = ProtoBuf.loadProtoFile("nyct-subway.proto.txt").build("transit_realtime");
 
 var callback = function(response) {
 	var data = [];
@@ -20,12 +27,18 @@ var callback = function(response) {
 	response.on('end', function () {
 		data = Buffer.concat(data);
 		var msg = transit.FeedMessage.decode(data);
-		console.log(msg);
+		var tripData = msg.entity;
+
+		filteredTripData = _.filter(tripData, function(tripData) {
+			if (tripData.trip_update) {
+				return (tripData.trip_update.trip.route_id == currentLine);
+			}
+		})
+
+		console.log("Number of trains currently running on " + currentLine + " line is " + filteredTripData.length);
+		mtaData = filteredTripData;
 	});
 }
-
-//Proto work
-var transit = ProtoBuf.loadProtoFile("nyct-subway.proto.txt").build("transit_realtime");
 
 //Run the server
 
@@ -34,7 +47,7 @@ var app = express()
 app.get('/', function(req, res) {
 
 	function TimeoutHandler() {
-		res.send("Hello");
+		res.send(mtaData);
 	}
 
 	http.request(options, callback).end();
